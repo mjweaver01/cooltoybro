eb332b0012c84e9905b7115e1a1e6275b21301b9
electric-mike <66960109+electric-mike@users.noreply.github.com>
2024-02-21 23:55:45 -0500

    partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-import react from "@astrojs/react";
-
+import partytown from '@astrojs/partytown'
 import netlify from "@astrojs/netlify";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), react()],
+  integrations: [
+    tailwind(), 
+    svelte(), 
+    sitemap(),
+		partytown({
+			config: {
+			  forward: ["dataLayer.push"],
+			},
+		}),],
   output: "server",
   server: {
     port: 3000
