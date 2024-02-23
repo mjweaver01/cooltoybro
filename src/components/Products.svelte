3b2556250e9357f89b0f6f03d87f20f7565fa196
electric-mike <66960109+electric-mike@users.noreply.github.com>
2024-02-23 16:14:16 -0500

    filter products

--- src/components/Products.svelte
+++ src/components/Products.svelte
@@ -4,12 +4,20 @@
   import ProductFlair from '@/components/ProductFlair.svelte'
   let products: ProductRecord[]
   let filter: string = ''
+  let filteredProducts: ProductRecord[] = products
+  let searchTerm = ''
+
+  const searchProducts = () => {
+    return (filteredProducts = products.filter((product) => {
+      return JSON.stringify(product).includes(searchTerm.toLowerCase())
+    }))
+  }
   export { products, filter }
 </script>
 
 {#if products.length <= 0}
-  <div class=" sm:gap-8 p-4 sm:p-8 max-w-screen-xl m-auto">
-    <div class="w-full rounded-xl mt-16">
+  <div class="sm:gap-8 p-4 sm:p-8 pb-0 max-w-screen-xl m-auto mt-16">
+    <div class="w-full rounded-xl">
       <h1 class="text-3xl font-bold">
         No toys found {#if filter.length > 0}under "{filter}"{/if}
       </h1>
@@ -19,12 +27,26 @@
 
 {#if products.length > 0}
   <div class="p-4 sm:p-8 max-w-screen-xl m-auto mt-16">
-    <h1 class="text-3xl font-bold mb-4 sm:mb-8">
+    <h1 class="text-3xl font-bold mb-4">
       {#if filter.length > 0}{filter}{/if}{#if filter.length <= 0}All{/if} toys
     </h1>
+    <div class="pb-4 sm:pb-8">
+      <input
+        type="text"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder={`Search ${filter || 'all'} toys`}
+        bind:value={searchTerm}
+        on:input={searchProducts}
+      />
+    </div>
 
     <div class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-8">
-      {#each products as product, i}
+      {#if filteredProducts.length == 0}
+        <h2 class="text-2xl font-500">
+          No results for "{searchTerm}"
+        </h2>
+      {/if}
+      {#each filteredProducts as product, i}
         <div class="w-full rounded-xl shadow-lg p-5 bg-white card">
           <div class="flex flex-col items-center justify-between h-full">
             <a href={`/product/${product.slug}`} class="w-full">
